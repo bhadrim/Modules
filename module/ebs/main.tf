8c2fd4b53ce4c07cfe22376b98ef60e4a43d595b
bhadrim <bhadrim@ca.ibm.com>
2020-02-13 14:11:40 -0500

    Create main.tf

---
+++ module/ebs/main.tf
@@ -0,0 +1,13 @@
+variable "password" {
+  default = "foo"
+}
+
+resource "camc_scriptpackage" "ec2_create" {
+  program = ["/bin/bash","./create.sh"]
+  program_sensitive = ["-p", "${var.password}"]
+  on_create = true
+}
+
+output "create_result" {
+  value = "${camc_scriptpackage.ec2_create.result}"
+}
